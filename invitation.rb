df0d41ff729d87c31c32610703e5e251562a5119
keylimotino <stephanie48758@gmail.com>
2018-06-04 21:08:27 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,15 @@
 # Code your prompts here!
+puts "What's your name?"
+guest_name = gets.strip
+puts "What's your party name?"
+party_name = gets.strip
+puts "What's the date"
+date = gets.strip
+puts = "What time is the party"
+time = gets.strip
+puts = "What's your host name?"
+host_name = gets.strip
+
+Hello #{guest_name}
 
 # Try starting out with puts'ing a string.
